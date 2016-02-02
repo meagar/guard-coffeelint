0e1b0ea72ceb312abf9d4fac8c23dd8e7f79ad21
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2016-02-02 11:01:32 -0800

    Started rework to handle multiple files

--- lib/guard/coffeelint.rb
+++ lib/guard/coffeelint.rb
@@ -1,14 +1,16 @@
-
 require 'guard'
 require 'guard/plugin'
-require 'coffeelint'
+require 'JSON'
+require 'colorize'
+# require 'coffeelint'
 
 module Guard
   class Coffeelint < Plugin
 
     def initialize(options = {})
       super
-      @config_file = options[:config_file] || 'config/coffeelint.json'
+      # @config_file = options[:config_file] || 'config/coffeelint.json'
+      @config_file = options[:config_file]
     end
 
     def start
@@ -21,28 +23,29 @@ module Guard
     # @return [Object] the task result
     #
     def run_on_changes(paths)
+      lint_and_report paths
       # Total errors across all files
-      error_count = 0
+      # error_count = 0
 
-      paths.each do |path|
-        errors = lint_and_report(path)
+      # paths.each do |path|
+      #   errors = lint_and_report(path)
 
-        error_count += errors.length
-      end
+      #   error_count += errors.length
+      # end
 
-      notify(paths, error_count)
+      # notify(paths, error_count)
 
-      true
+      # true
+    end
+
+    def run_all
+      lint_and_report
     end
 
     protected
 
-    def notify(paths, error_count)
-      message = if paths.length == 1
-        "#{error_count} errors in #{paths[0]}"
-      else
-        "#{error_count} errors in #{paths.length} files"
-      end
+    def notify(file_count, error_count)
+      message =  "#{error_count} errors in #{file_count} files"
 
       image = if error_count > 0
         :failed
@@ -50,28 +53,85 @@ module Guard
         :success
       end
 
-      Notifier.notify(message, title: "Coffeelint", image: image)
+      summary = "#{file_count} files scanned, "
+      summary << if error_count > 0
+                   "#{error_count} errors"
+                 else
+                   "No errors"
+                 end
+      summary << " found."
+
+      Notifier.notify(summary, title: "Coffeelint", image: image)
     end
 
 
-    def lint_and_report(path)
+    def lint_and_report(paths = nil)
       # This is reporting some bad false-positives
-      #errors = ::Coffeelint.lint_file(path, config_file: @config_file)
+      # errors = ::Coffeelint.lint_file(path, config_file: @config_file)
 
       # This works :|
-      errors = JSON.parse(`coffeelint --reporter raw -f #{@config_file} #{path}`).values.first
+      command = 'coffeelint -c --reporter raw'
+      command += "-f #{@config_file}" if @config_file
+      command += if paths && paths.length > 0
+                   " #{paths.join ' '}"
+                 else
+                   ' .'
+                 end
 
-      if errors.length > 0
-        UI.warning "Coffeelint: #{path} has #{errors.length} errors"
+      puts command
+      results = `#{command}`
+      puts results
+      puts $?.success?
 
-        errors.each do |error|
-          UI.warning "#{error['lineNumber']}: #{error['message']} (#{error['context']})"
-        end
-
+      begin
+        results = JSON.parse(results)
+      rescue JSON::ParserError
+        UI.error "Error linting #{paths}"
+        return
       end
 
-      errors
-    end
+      results.each do |file, errors|
+        errors.each do |error|
+          error_letter = error['level'].chars.first.upcase
+          error_letter = case error_letter
+                         when 'E' then error_letter.red
+                         when 'W' then error_letter.yellow
+                         else error_letter
+                         end
 
+          puts "#{file.cyan}:#{error['lineNumber']}:#{error_letter}" \
+            ": #{error['name']}: #{error['message']}.#{error['context']}"
+        end
+      end
+
+      file_count = results.size
+      error_count = results.map { |_, e| e.size }.reduce(:+)
+      summary = "#{file_count} files scanned, "
+      summary << if error_count > 0
+                   "#{error_count} errors".red
+                 else
+                   "No errors".green
+                 end
+      summary << " found."
+
+      puts "\n" + summary
+
+      notify file_count, error_count
+
+      throw :task_has_failed unless error_count == 0
+
+      # errors = JSON.parse(`coffeelint --reporter raw -f #{@config_file} #{path}`).values.first
+
+    #   if errors.length > 0
+    #     UI.warning "Coffeelint: #{path} has #{errors.length} errors"
+
+    #     errors.each do |error|
+    #       UI.warning "#{error['lineNumber']}: #{error['message']} (#{error['context']})"
+    #     end
+
+    #   end
+
+    #   errors
+    end
   end
 end
